085f4e711bf4e5b1f16d674c44f5b32913dde960
ChoiHyunSan <chsan626@gmail.com>
2025-08-12 01:17:41 +0900

    [CHORE] Redis 패키지 및 도커 이미지 세팅 추가

--- APIServer/APIServer/Repository/DbConfig.cs
+++ APIServer/APIServer/Repository/DbConfig.cs
@@ -5,9 +5,9 @@
 /// </summary>
 public class DbConfig
 {
-    public string  AccountDb { get; set; }
-    public string  GameDb { get; set; }
-    public string  MasterDb { get; set; }
-    
-    public string  Redis { get; set; }
+    public string  AccountDb { get; set; } = string.Empty;
+    public string GameDb { get; set; } = string.Empty;
+    public string MasterDb { get; set; } = string.Empty;
+
+    public string Redis { get; set; } = string.Empty;
 }
---
+++ APIServer/APIServer/Repository/RedisBase.cs
@@ -0,0 +1,14 @@
+﻿using CloudStructures;
+
+namespace APIServer.Repository;
+
+public abstract class RedisBase
+{
+    protected RedisConnection _conn;
+
+    protected RedisBase(string redisConfig)
+    {
+        var config = new RedisConfig("default", redisConfig);
+        _conn = new RedisConnection(config);
+    }
+}
