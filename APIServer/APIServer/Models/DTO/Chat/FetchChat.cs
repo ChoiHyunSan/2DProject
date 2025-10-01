7d99cbd63d9be4a315c0335b9d79a9eccef918ec
ChoiHyunSan <chsan626@gmail.com>
2025-10-01 17:57:47 +0900

    [FEAT] 채팅 관련 DTO 클래스 추가

---
+++ APIServer/APIServer/Models/DTO/Chat/FetchChat.cs
@@ -0,0 +1,19 @@
+﻿namespace APIServer.Models.DTO.Chat;
+
+public class FetchChatRequest : RequestBase
+{
+    public string latest { get; set; } = string.Empty;
+    public int limit { get; set; }
+}
+
+public class FetchChatResponse : RequestBase
+{
+    public List<ChatMessage> messages { get; set; } = [];
+}
+
+public class ChatMessage
+{
+    public string messageId { get; set; } = string.Empty;
+    public string email { get; set; } = string.Empty;
+    public string message { get; set; } = string.Empty;
+}
---
+++ APIServer/APIServer/Models/DTO/Chat/SendChat.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIServer.Models.DTO.Chat;
+
+public class SendChatResponse : ResponseBase
+{
+
+}
+
+public class SendChatRequest : RequestBase
+{
+    [Required]
+    [MinLength(1, ErrorMessage = "CHAT MESSAGE CANNOT BE EMPTY")]
+    private string message { get; set; } = string.Empty;    
+}
